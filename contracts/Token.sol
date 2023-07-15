b399c10f58fe43587dc7c404333d4f49ad42823c
LSNM <54871871+LSNMCLNTL@users.noreply.github.com>
2023-07-15 18:17:20 +0800

    Add files via upload

---
+++ contracts/Token.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.3.0/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    address private owner;
+
+    constructor() ERC20("Chocolate", "CHOC") {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the contract owner can call this function");
+        _;
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        require(amount <= balanceOf(msg.sender), "Insufficient balance");
+        return super.transfer(recipient, amount);
+    }
+}
